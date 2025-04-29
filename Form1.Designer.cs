aee365d53746fd9c54af7beea25bf0b4ede677fa
e7kandil <e7kandil@gmail.com>
2025-04-29 23:01:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsApplication26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.BurlyWood;
+            this.label1.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1277, 191);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "تقديم بيانات العملاء";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.BurlyWood;
+            this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(967, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(630, 133);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "الاسم كامل";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.BurlyWood;
+            this.label3.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(968, 484);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(629, 127);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "الرقم القومي";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.BurlyWood;
+            this.label4.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(963, 689);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(634, 143);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "رقم الهاتف";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.BurlyWood;
+            this.label5.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(957, 935);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(639, 132);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "العنوان";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.BurlyWood;
+            this.label6.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(955, 1165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(642, 133);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "اسم المحكمه التابع لها";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.BackColor = System.Drawing.Color.BurlyWood;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(1210, 1390);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.radioButton1.Size = new System.Drawing.Size(386, 141);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "شاكي";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.BackColor = System.Drawing.Color.BurlyWood;
+            this.radioButton2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(656, 1390);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.radioButton2.Size = new System.Drawing.Size(411, 141);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "مشكو منه";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.BurlyWood;
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "قضيه اسريه",
+            "قضيه مدنيه",
+            "قضيه جنح",
+            "قضيه جنايات"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 1411);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(430, 101);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BurlyWood;
+            this.button1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(584, 1597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(558, 145);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "حفظ البيانات";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(73, 294);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(821, 76);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(73, 517);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(821, 76);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(73, 730);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(821, 76);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(73, 971);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(821, 76);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(73, 1201);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(821, 76);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = global::WindowsFormsApplication26.Properties.Resources.Law_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(2419, 1601);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Text = "تقديم بيانات العملاء";
+            this.Size = new Size(1277, 191);
+            this.BackColor = Color.BurlyWood;
+            label1.Font = new Font("Arial", 36, FontStyle.Bold);
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Location = new Point(87, 9);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.Law_background;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.BackColor = Color.BurlyWood;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            this.Size = new Size(630, 133);
+            label2.Location = new Point(967, 267);
+            this.Text = "الاسم كامل";
+            label2.Location = new Point(textBox1.Left + textBox1.Width +25, textBox1.Top);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.BackColor = Color.BurlyWood;
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            this.Size = new Size(630, 133);
+            label3.Location = new Point(968, 484);
+            this.Text = "الرقم القومي";
+            label3.Location = new Point(textBox2.Left + textBox2.Width + 25, textBox2.Top);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            label4.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.BackColor = Color.BurlyWood;
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            this.Size = new Size(630, 133);
+            label4.Location = new Point(963, 689);
+            this.Text = "رقم الهاتف";
+            label4.Location = new Point(textBox3.Left + textBox3.Width + 25, textBox3.Top);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            label5.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.BackColor = Color.BurlyWood;
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            this.Size = new Size(630, 133);
+            label5.Location = new Point(957, 935);
+            this.Text = "العنوان";
+            label5.Location = new Point(textBox4.Left + textBox4.Width + 25, textBox4.Top);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            label6.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.BackColor = Color.BurlyWood;
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            this.Size = new Size(630, 133);
+            label6.Location = new Point(955, 1165);
+            this.Text = "اسم المحكمه التابع لها";
+            label6.Location = new Point(textBox5.Left + textBox5.Width + 25, textBox5.Top);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font("Arial", 18, FontStyle.Bold);
+            textBox1.BackColor = Color.BurlyWood;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Font = new Font("Arial", 18, FontStyle.Bold);
+            textBox2.BackColor = Color.BurlyWood;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            textBox2.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Font = new Font("Arial", 18, FontStyle.Bold);
+            textBox3.BackColor = Color.BurlyWood;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.TextAlign = HorizontalAlignment.Right;
+            textBox3.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            textBox4.Font = new Font("Arial", 18, FontStyle.Bold);
+            textBox4.BackColor = Color.BurlyWood;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            textBox4.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Font = new Font("Arial", 18, FontStyle.Bold);
+            textBox5.BackColor = Color.BurlyWood;
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.TextAlign = HorizontalAlignment.Right;
+            textBox5.RightToLeft = RightToLeft.Yes;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Font = new Font("Arial", 24, FontStyle.Bold);
+            radioButton1.BackColor = Color.BurlyWood;
+            radioButton1.RightToLeft = RightToLeft.Yes;
+            this.Text = "شاكي";
+            
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton2.Font = new Font("Arial", 24, FontStyle.Bold);
+            radioButton2.BackColor = Color.BurlyWood;
+            radioButton2.RightToLeft = RightToLeft.Yes;
+            this.Text = "مشكو منه";
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Font = new Font("Arial", 24, FontStyle.Bold);
+            comboBox1.BackColor = Color.BurlyWood;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.RightToLeft = RightToLeft.Yes;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Font = new Font("Arial", 24, FontStyle.Bold);
+            button1.BackColor = Color.BurlyWood;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Cursor = Cursors.Hand;
+        }
+    }
+}
